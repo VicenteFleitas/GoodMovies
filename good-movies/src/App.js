2249f5dd1110a91c1fe98cbc614e89f1bf2f84d7
VicenteFleitas <vicente1fleitas1@gmail.com>
2022-02-15 09:25:23 -0300

    add login

--- good-movies/src/App.js
+++ good-movies/src/App.js
@@ -4,6 +4,9 @@ import { Modal, Button, Form, Card, Row, Col } from "react-bootstrap";
 
 import Layout from "./components/Layout";
 import NavigationBar from "./components/NavigationBar";
+import { login } from "./lib/login";
+
+import loginLogo from "./assets/loginlogo.jpg";
 
 function App() {
   // verify authentication
@@ -24,6 +27,26 @@ function App() {
   const emailRef = useRef();
   const passwordRef = useRef();
 
+  // login
+  const handleLogin = () => {
+    handleClose();
+    login({
+      email: emailRef.current.value,
+      pass: passwordRef.current.value,
+    }).then((result) => {
+      if (result.access) {
+        setUserId(result.access);
+        console.log("login success!");
+        setIsConnected(true);
+      } else {
+        //handle erros
+        if (result.detail) console.log(result.detail);
+        if (result.username) console.log(result.username);
+        if (result.password) console.log(result.password);
+      }
+    });
+  };
+
   return (
     <>
       <Layout>
@@ -35,6 +58,43 @@ function App() {
             userId={userId}
           />
         </Router>
+        {/* Modal para login */}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Ingresa Ahora</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Card>
+              <Card.Body className="text-center">
+                <img
+                  alt=""
+                  src={loginLogo}
+                  width="300"
+                  //className="d-inline-block align-top"
+                />
+                <h4 className="text-center mb-4 mt-4">Iniciar sesión</h4>
+                <Form>
+                  <Form.Group id="email">
+                    <Form.Label>Usuario</Form.Label>
+                    <Form.Control type="email" ref={emailRef} required />
+                  </Form.Group>
+                  <Form.Group id="password">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" ref={passwordRef} required />
+                  </Form.Group>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cerrar
+            </Button>
+            <Button variant="primary" onClick={handleLogin}>
+              Acceder
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Layout>
     </>
   );
---
+++ good-movies/src/lib/login.js
@@ -0,0 +1,26 @@
+export function login(props) {
+  let result = new Promise((resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      username: props.email,
+      password: props.pass,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://movies.z4.tdplab.com/api/token/", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((error) => console.log("error", error));
+  });
+  return result;
+}
